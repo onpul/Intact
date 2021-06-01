b8febd69b5a9ce29cf788677eafae8279a8a00bf
onpul <onpul__@naver.com>
2021-06-01 22:06:38 +0900

    2021.06.01.(2)

--- php/upload.php
+++ php/upload.php
@@ -9,229 +9,7 @@
 	<link rel="stylesheet" href="css/index.css?ver=1">
 	<link rel="stylesheet" href="css/default.css?ver=1">
 	<link rel="stylesheet" href="css/layout.css?ver=1">
-	<style>
-		.container{
-			padding: 30px;
-			display: grid;
-			/* width: 100%; */
-			grid-template-columns: 1fr 800px 1fr;
-			grid-template-rows: auto;
-			row-gap: 30px;
-			margin-bottom: 70px;
-		}
-		/* 메뉴 탑 */
-		.area:nth-child(1) {
-			grid-column: 2 / 3;
-			grid-row: 1 / 2;
-			justify-content: center;
-			align-items: center;
-			display: flex;
-			padding-top: 50px;
-			padding-left: 20px;
-			padding-right: 20px;
-			margin-bottom: 40px;
-		}
-		/* 왼쪽 사이드 메뉴 */
-    .area:nth-child(2) {
-    	grid-column: 1 / 2;
-    	grid-row: 1 / 4;
-      justify-self: start;
-			position: fixed;
-    }
-		/* 메인 */
-    .area:nth-child(3) {
-    	grid-column: 2 / 3;
-      justify-self: center;
-      text-align: center;
-    }
-		/* 상단 메인 이미지 업로드 박스 */
-		.main-img-box form {
-			border: 1px solid #000000;
-			border-radius: 10px;
-			margin: 5px;
-			display: flex;
-			justify-content: center;
-			height: 50px;
-		}
-		.main-img-box{
-			text-align: center;
-			margin-right: auto;
-			padding-right: 15px;
-			border-radius: 10px;
-			float: left;
-			width: 250px;
-			overflow: hidden;
-			border: none;
-		}
-		.main-img-div{
-			position: relative;
-			height: auto;
-			width: 150px;
-		}
-		.input-button{
-			width: 87px;
-			height: 50px;
-			position: absolute;
-			top: 0px;
-			left: 0px;
-			color: #ffffff;
-			border: none;
-			background-color: #000000;
-      border-radius: 10px;
-      cursor: pointer;
-		}
-		.input-hidden{
-			width: 87px;
-			height: 50px;
-			position: absolute;
-			top: 0px;
-			left: 0px;
-			opacity: 0;
-			cursor: pointer;
-		}
-		.category-box {
-			text-align: center;
-			background: #F5F5F5;
-			width: 245px;
-			height: 50px;
-			border-radius: 10px;
-
-		}
-		.category-box form {
-			line-height: 50px;
-		}
-		.upload-summit input{
-			height: 50px;
-			width: 100px;
-			cursor: pointer;
-			background-color: #000000;
-			border: 0;
-			border-radius: 10px;
-			color: #ffffff;
-			margin-left: 10px;
-		}
-		.menu-side {
-      text-align: center;
-			z-index: 50;
-			color: #ffffff;
-    }
-    .menu-side ul > li {
-      margin-bottom: 10px;
-      background: #ffffff;
-      width: 30px;
-      height: 100px;
-      line-height: 100px;
-      border-radius: 10px;
-			box-shadow: 0px 0px 6px gray;
-    }
-		.menu-side a {
-			color: #ffffff;
-		}
-		.menu-side ul > li:nth-child(1) {
-			background: #FF7F00;
-			color: #ffffff;
-		}
-		.upload-container {
-      height: 400px;
-      width: 700px;
-      background: #FFFFFF;
-      border-radius: 20px;
-			box-shadow: 0px 0px 15px gray;
-			margin-bottom: 40px;
-			padding: 30px;
-    }
-		.box {
-			height: 400px;
-      width: 700px;
-			border-radius: 20px;
-			/* border: 1.5px dashed #707070; */
-		}
-    input:focus {outline:none;}
-    textarea:focus {outline:none;}
-		.img-area {
-			width: 100%;
-			height: 40px;
-			margin-bottom: 20px;
-		}
-    .img-area form{
-    	display: flex;
-			justify-content: center;
-			padding-top: 20px;
-			width: 100%;
-			height: 40px;
-		}
-		.file-input-textbox{
-			text-align: right;
-			margin-right: 20px;
-			padding-right: 5px;
-			float: left;
-			width: 250px;
-			overflow: hidden;
-			border: none;
-		}
-		.file-input-div{
-			position: relative;
-			width: 150px;
-			height: auto;
-		}
-		.file-input-button{
-			width: 150px;
-			height: 40px;
-			position: absolute;
-			top: 0px;
-			left: 0px;
-			color: #ffffff;
-			border: none;
-			background-color: #FF7A24;
-			box-shadow: 0px 1px 10px gray;
-      border-radius: 10px;
-      cursor: pointer;
-		}
-		.file-input-hidden{
-			width: 150px;
-			height: 40px;
-			position: absolute;
-			top: 0px;
-			left: 0px;
-			opacity: 0;
-			cursor: pointer;
-		}
-		.content-form form {
-			padding-top: 20px;
-		}
-		.content-form form #name {
-			width: 500px;
-			border: none;
-			border-radius: 10px 10px 0px 0px;
-			/* background: #F5F5F5; */
-			border-bottom: 1px solid #E0E0E0;
-			padding: 5px;
-		}
-		.content-form form #content {
-			width: 500px;
-			border: none;
-			border-radius: 0px 0px 10px 10px;
-			/* background: #F5F5F5; */
-			margin-top: 20px;
-			padding: 5px;
-			height: 250px;
-		}
-		.add-item {
-			height: 40px;
-			width: 100px;
-			cursor:pointer;
-			background-color: #ffffff;
-			border:0;
-			border-radius: 10px;
-			color: #20AC20;
-			box-shadow: 0px 1px 10px gray;
-			margin-bottom: 10px;
-		}
-		.button-group {
-			display: flex;
-			justify-content: center;
-		}
-	</style>
+	<link rel="stylesheet" href="css/upload.css">
 </head>
 <body>
   <!-- 메인 컨테이너 시작(바디) -->
@@ -277,7 +55,8 @@
 				<div class="top-form area">
 					<!-- 전시회 메인 사진 -->
 					<div class="main-img-box">
-						<form>
+						<!-- 폼1: 전시회 메인 사진 -->
+						<form name="main-img" method="get" action="">
 							<input type="text" id="fileName1" class="main-img-box" readonly="readonly" placeholder="전시회 메인 사진" />
 							<div class="main-img-div" id="main-img-container">
 								<input type="button" value="첨부" class="input-button" />
@@ -285,11 +64,18 @@
 							</div>
 						</form>
 					</div>
-					<div class="category-box">
+					<div class="category-box"><a name="main"></a>
+						<!-- 폼2: 전시회 카테고리 -->
 						<form name="category" method="get" action="">
-							<div class="select">
-								<input type="radio" id="select" name="category" value="category1" checked><label for="select" class="box-radio-input">디지털</label>
-								<input type="radio" id="select2" name="category" value="category2"><label for="select2" class="box-radio-input">회화</label>
+							<div class="checks">
+								<ul>
+									<li>
+										<input type="radio" class="rd1" id="rd1" name="category" value="category1" checked><label for="rd1">회화</label>
+									</li>
+									<li>
+										<input type="radio" class="rd2" id="rd2" name="category" value="category2"><label for="rd2">디지털</label>
+									</li>
+								</ul>
 							</div>
 						</form>
 					</div>
@@ -298,44 +84,28 @@
 				<!-- 사이드 메뉴 -->
         <div class="menu-side area">
           <ul>
-            <li><a href="#main">top</a></li>
+            <li><a href="#main">TOP</a></li>
           </ul>
         </div>
 				<div class="area">
 					<div class="upload-container">
 						<!-- 박스 시작 -->
 						<div class="box">
-							<div class="img-area">
-								<!-- 전시회 사진(이미지) -->
-								<!-- <form name="image" action="" method="post">
-									<div id="image-container">
-										<input type="file" id="image" accept="image/*" style="display:none">
-										<label class="input-file-button" for="image">파일 선택</label>
-									</div>
-								</form> -->
-								<form name="image" action="" method="post">
-									<input type="text" id="fileName" class="file-input-textbox" readonly="readonly" placeholder="첨부된 파일이 없습니다.">
-									<div class="file-input-div" id="image-container">
-										<input type="button" value="파일 첨부" class="file-input-button">
-										<input type="file" id="image" accept="image/*" class="file-input-hidden" onchange="javascript:document.getElementById('fileName').value = this.value.split('\\')[this.value.split('\\').length-1]">
-									</div>
-								</form>
-							</div>
-
 							<!-- 텍스트 아레아 시작 -->
 							<div class="text-area">
 								<div class="content-form">
 									<!-- 전시회 내용(텍스트) -->
+									<!-- 폼3: 전시회 제목 및 내용 -->
 									<form name="content" enctype="multipart/form-data" action="" method="post">
 										<ul>
 											<li>
 												<!-- 전시회 제목 -->
-												<input id="name" type="text" name="name" placeholder="전시회 제목"></a>
+												<input id="name" type="text" name="name" placeholder="전시회 제목을 써주세요"></a>
 											</li>
 										</ul>
 										<ul>
 											<!-- 전시회 소개 및 목차 -->
-											<li><textarea id="content" cols="70" rows="14" name="content" placeholder="전시회 소개 및 목차" style="resize:none;"></textarea></li>
+											<li><textarea id="content" cols="70" rows="14" name="content" placeholder="전시회 소개글을 써주세요" style="resize:none;"></textarea></li>
 										</ul>
 									</form>
 								</div>
@@ -346,55 +116,106 @@
 					</div>
 					<!-- 프리셋 시작 -->
 					<div id="pre_set">
-						<div class="upload-container">
+						<div class="preset-container">
 							<!-- 박스 시작 -->
 							<div class="box">
-								<div class="img-area">
-									<!-- 전시회 사진(이미지) -->
-									<!-- <form name="image" action="" method="post">
-										<div id="image-container">
-											<input type="file" id="image" accept="image/*" style="display:none">
-											<label class="input-file-button" for="image">파일 선택</label>
-										</div>
-									</form> -->
-									<form name="image" action="" method="post">
-										<input type="text" id="fileName" class="file-input-textbox" readonly="readonly" placeholder="첨부된 파일이 없습니다.">
-										<div class="file-input-div" id="image-container">
-										  <input type="button" value="파일 첨부" class="file-input-button">
-										  <input type="file" id="image" accept="image/*" class="file-input-hidden" onchange="javascript:document.getElementById('fileName').value = this.value.split('\\')[this.value.split('\\').length-1]">
-										</div>
-									</form>
-								</div>
+								<!-- 폼4: 작품 제목, 이미지, 내용, 기간 등 -->
+								<form name="art-content" method="get" action="">
+									<!-- 작품 제목 -->
+									<div class="title area1">
+										<input id="name" type="text" name="name" placeholder="작품 제목을 써주세요">
+									</div>
+									<!-- 작품 제목 끝 -->
 
-								<!-- 텍스트 아레아 시작 -->
-								<div class="text-area">
-									<div class="content-form">
-										<!-- 작품 내용(텍스트) -->
-										<form name="content" enctype="multipart/form-data" action="" method="post">
-											<ul>
+									<!-- 작품 이미지  -->
+									<div class="img-box area1">
+										<div class="art-img">
+											<div id="image_container"></div>
+											<label for="artimg">파일 찾기</label>
+			                <input type="file" id="artimg" accept="image/*" style=""/>
+											<p>권장사항 : 1MB 이하 고화질 .jpg, .png 파일</p>
+										</div>
+									</div>
+									<!-- 작품 이미지 끝 -->
+
+									<!-- 작품 내용 -->
+									<div class="content-box area1">
+										<!-- 미판매일 경우의 폼 -->
+										<div class="art-content">
+											<textarea id="content" cols="70" rows="14" name="content" placeholder="작품 소개글을 써주세요" style="resize:none;"></textarea>
+										</div>
+
+										<!-- 판매일 경우의 폼 -->
+										<!-- <div class="art-content">
+											<textarea id="content-sell" cols="70" rows="14" name="content-sell" placeholder="작품 소개글을 써주세요" style="resize:none;"></textarea>
+											<ul class="sell-price">
 												<li>
-													<!-- 작품 제목 -->
-													<input id="name" type="text" name="name" placeholder="작품 제목"></a>
+													<label for="content-sell-price">작품 가격</label>
+												</li>
+												<li>
+													<input id="content-sell-price" type="text" name="content-sell" placeholder="작품 가격을 입력하세요" />
 												</li>
 											</ul>
-											<ul>
-												<!-- 작품 소개 및 목차 -->
-												<li><textarea id="content" cols="70" rows="14" name="content" placeholder="작품 소개" style="resize:none;"></textarea></li>
+										</div> -->
+
+										<!-- 경매일 경우의 폼 -->
+										<!-- <div class="art-content">
+											<textarea id="content-auc" cols="70" rows="14" name="content-auc" placeholder="작품 소개글을 써주세요" style="resize:none;"></textarea>
+											<ul class="sell-price">
+												<li>
+													<label for="content-sell-price">경매 시작가</label>
+												</li>
+												<li>
+													<input id="content-sell-price" type="text" name="content-sell" placeholder="경매 시작가를 입력하세요. 100,000원 이하" />
+												</li>
+												<li>
+													<label for="content-sell-price">응찰 단위 (원)</label>
+												</li>
+												<li>
+													<input id="content-sell-price" type="text" name="content-sell" placeholder="응찰 단위를 입력하세요. 10,000원 이하" />
+												</li>
+												<ul>
+													<li>
+														<label for="content-sell-price">경매 진행 기간</label>
+													</li>
+													<li>
+														<input id="content-auc-date" type="datetime-local" name="start"/>부터<br>
+														<input id="content-auc-date" type="datetime-local" name="end"/>까지
+													</li>
+												</ul>
 											</ul>
+										</div> -->
+									</div>
+								</form>
+								<!-- 작품 내용 끝 -->
+
+								<!-- 판매 옵션 설정 -->
+								<div class="sell-opt-box area1">
+									<p>작품 판매 방법 선택</p>
+									<div class="sell-chk">
+										<!-- 폼5: 작품 판매 옵션 선택 -->
+										<form name="category" method="get" action="">
+											<div class="select">
+												<input type="radio" id="select" name="sell-opt" value="category1" checked><label for="select" class="box-radio-input">미판매</label>
+												<input type="radio" id="select2" name="sell-opt" value="category2"><label for="select2" class="box-radio-input">판매</label>
+												<input type="radio" id="select3" name="sell-opt" value="category3"><label for="select3" class="box-radio-input">경매</label>
+											</div>
 										</form>
 									</div>
 								</div>
-								<!-- 텍스트 아레아 끝 -->
+								<!-- 판매 옵션 설정 끝 -->
+
 							</div>
 							<!-- 박스 끝 -->
 						</div>
+						<!-- 프리셋 컨테이너 끝 -->
 					</div>
 					<!-- 프리셋 끝 -->
 					<div id="field"></div>
 					<!-- <input type="button" value="삭제" onclick="remove_item(this)"> -->
 					<ul class="button-group">
 						<li>
-							<input class="add-item" type="button" value="추가" onclick="add_item()">
+							<input class="add-item" type="button" value="추가">
 						</li>
 					</ul>
 				</div>
@@ -410,19 +231,5 @@
 		<!-- 푸터 끝-->
 	</div>
 	<!-- 메인 컨테이너 끝(바디 바깥 쪽) -->
-		<script>
-    	function add_item(){
-      	// pre_set 에 있는 내용을 읽어와서 처리
-        var div = document.createElement('div');
-          div.innerHTML = document.getElementById('pre_set').innerHTML;
-          document.getElementById('field').appendChild(div);
-	     }
-	     // function remove_item(obj){
-	     //      // obj.parentNode 를 이용하여 삭제
-	     //      document.getElementById('field').removeChild(obj.parentNode);
-	     // }
-
-			 function btn(){ alert('업로드 되었습니다.'); }
-		</script>
 </body>
 </html>
--- purchase-login.html
+++ purchase-login.html
@@ -1,4 +1,4 @@
-<!-- 로그인 후 구매 페이지 -->
+<!-- 구매 페이지 -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Intact</title>
-	<link rel="stylesheet" href="css/index.css?ver=1">
 	<link rel="stylesheet" href="css/layout.css?ver=1">
 	<link rel="stylesheet" href="css/default.css?ver=1">
   <style>
@@ -30,69 +29,92 @@
     .menu li a:hover{text-decoration: underline;}
 
     /* 구매 페이지 스타일 속성 */
-    .container {
-      padding: 50px;
-      display: grid;
-      grid-template-columns: 30px 170px auto 30px 100px;
-      grid-template-rows: auto;
-      row-gap: 20px;
-      align-items: center;
-      justify-items: start;
+    .grid-box{
+    	display: grid;
+			grid-template-rows: 220px 220px 220px;;
+		}
+		.area:nth-child(1) {
+      grid-row: 1 / 2;
+			background: #ffffff;
+			justify-self: center;
+			align-self: end;
     }
-    .item:nth-child(1) {
-      height: 40px;
-    	grid-column: 1 / 6;
+		.area:nth-child(2) {
+      grid-row: 2 / 3;
+			background: #FAFAFA;
+			justify-self: center;
+			align-self: center;
+			width: 100%;
     }
-    .item:nth-child(2) {
-      height: 40px;
-      grid-column: 1 / 6;
+		.area:nth-child(3) {
+      grid-row: 3 / 4;
+			background: #ffffff;
+			justify-self: center;
+			align-self: center;
     }
-    .item:nth-child(18) {
-      height: 40px;
-      grid-column: 1 / 6;
-    }
-    .item:nth-child(19) {
-      width: 100%;
-      grid-column: 1 / 4;
-      grid-row: 7 / 8;
-      text-align: center;
-      justify-self: center;
-      justify-content: space-around;
-    }
-		.item:nth-child(22) {
-      height: 40px;
-      grid-column: 1 / 6;
-			text-align: center;
-      justify-self: center;
-			height: 40px;
-    }
-    .product{
-      width: 100%;
-      width: 150px;
-      height: 150px;
-      overflow: hidden;
-    }
-    .product-img{
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-    .product-info{
-      margin-left: 40px;
-      width: 200px;
-      text-align: left;
-    }
-    .total-cost{
-      display: flex;
-    }
-    .updown { border: 0.1px solid #eee; width: 0px; height: 100px; }
-		#purchase-btn {
-			width: 200px;
-			height: 60px;
+		.title p {
+			height: 100px;
+			width: 900px;
+			font-weight: bold;
+			font-size: 15px;
+		}
+		.info {
+			margin-top: 30px;
+			margin-bottom: 30px;
+			background: #ffffff;
+			margin-left: auto;
+			margin-right: auto;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			width: 900px;
+			height: 150px;
 			border-radius: 10px;
-			border: 1px solid #FFD62C;
-			background-color: #FFD62C;
-			color: black;
+		}
+		.profile-img {
+		  width: 150px;
+		  height: 150px;
+		  border-radius: 10px 0px 0px 10px;
+		  overflow: hidden;
+		}
+		#profile-img {
+		  width: 100%;
+		  height: 100%;
+		  object-fit: cover;
+		}
+		.art-info {
+			width: 600px;
+			position: relative;
+			height: 130px;
+			border-right: 2px solid #777777;
+		}
+		.art-info ul {
+			position: absolute;
+			margin-left: 20px;
+			top: 50%;
+			transform: translate(0%, -50%);
+		}
+		.art-info ul #name {
+			margin-bottom: 10px;
+		}
+		.price {
+			width: 100px;
+			margin-left: 40px;
+		}
+		.price ul {
+			height: 100px;
+		}
+		.price ul #price {
+			font-weight: bold;
+			margin-bottom: 60px;
+		}
+		#purchase-btn {
+			width: 150px;
+			height: 50px;
+			border-radius: 10px;
+			border: 1px solid #000000;
+			background-color: #000000;
+			color: #ffffff;
 			padding: 5px;
 			cursor: pointer;
 		}
@@ -100,214 +122,84 @@
 </head>
 <body>
 	<div class="main-container">
-    <!-- 헤더 시작 -->
-  	<header class="header">
-  		<!-- 로고 -->
-  	  <div class="logo">
-  			<a href="./index-login.html">
-  			<img src="./images/main_logo.png" alt="Intact">
-  			</a>
-  	  </div>
-  	  <!-- 네비게이션1 -->
-  		<nav class="nav">
-  			<ul class="mainmenu">
-  				<li><a href="auction-login.html" class="menu-link">경매
-  					<ul class="submenu">
-  						<li><a href="#">그래픽</a></li>
-  						<li><a href="#">회화</a></li>
-  					</ul>
-  				</a></li>
-  				<li><a href="exhibit-login.html" class="menu-link">전시회
-  					<ul class="submenu">
-  						<li><a href="#">그래픽</a></li>
-  						<li><a href="#">회화</a></li>
-  					</ul>
-  				</a></li>
-  			</ul>
-  	  </nav>
-  	  <!-- 검색창 -->
-  	  <form class="search-form" action="search.php">
-  	  <input type="search">
-  	  </form>
-  	  <!-- 네비게이션2 -->
-  	  <nav class="nav">
-  	  	<li><a href="mypage.html" class="menu-link">마이페이지</a></li>
-  	  </nav>
-  	</header>
-  	<!-- 헤더 끝 -->
+		<!-- 헤더 시작 -->
+		<header class="header">
+			<!-- 로고 -->
+		  <div class="logo">
+				<a href="./index-login.html">
+				<img src="./images/main_logo.png" alt="Intact">
+				</a>
+		  </div>
+		  <!-- 네비게이션1 -->
+			<nav class="nav">
+				<ul class="mainmenu">
+					<li><a href="auction-login.html" class="menu-link">경매
+						<ul class="submenu">
+							<li><a href="#">그래픽</a></li>
+							<li><a href="#">회화</a></li>
+						</ul>
+					</a></li>
+					<li><a href="exhibit-login.html" class="menu-link">전시회
+						<ul class="submenu">
+							<li><a href="#">그래픽</a></li>
+							<li><a href="#">회화</a></li>
+						</ul>
+					</a></li>
+				</ul>
+		  </nav>
+		  <!-- 검색창 -->
+		  <form class="search-form" action="search.php">
+		  <input type="search">
+		  </form>
+		  <!-- 네비게이션2 -->
+		  <nav class="nav">
+		  	<li><a href="mypage.html" class="menu-link">마이페이지</a></li>
+		  </nav>
+		</header>
+		<!-- 헤더 끝 -->
 		<!-- 메인 시작 -->
 		<div class="main">
-      <!-- 메뉴바 -->
-      <ul class="menu">
-  			<li class="menu-item">
-  				<a href="mypage-exhibit.html" class="menu-link">내 프로필</a>
-  			</li>
-  			<li class="menu-item">
-  				<a href="scraping.html" class="menu-link">스크랩</a>
-  			</li>
-  		</ul>
-      <!-- 메인 컨테이너 -->
-      <div class="container">
-        <div class="item">
-          <ul class="count">
-            <li>장바구니 총 3개</li>
-          </ul>
-        </div>
-        <div class="item">
-          <input type="checkbox" id="all_chk"><label for="all_chk">전체 상품 선택</label>
-        </div>
-        <div class="item">
-          <div class="check">
-            <input type="checkbox" id="product_chk"><label for="product_chk"></label>
-          </div>
-        </div>
-      	<div class="item">
-          <p>작가 이름</p>
-          <div class="product">
-            <img class="product-img" src="https://i.pinimg.com/originals/cf/34/f0/cf34f0af5496bf6eadda07c8042b9279.png" alt="상품 사진">
-          </div>
-        </div>
-      	<div class="item">
-          <div class="product-info">
-            <ul>
-              <li>작품명</li>
-              <li>작가명</li>
-              <li>작품형식</li>
-            </ul>
-          </div>
-        </div>
-      	<div class="item">
-            <div class="updown"></div>
-        </div>
-      	<div class="item">
-          <div class="cost">
-            <ul>
-              <li>가격</li>
-              <li>작품 가격</li>
-              <li>배송비</li>
-            </ul>
-          </div>
-        </div>
-        <div class="item">
-          <div class="check">
-            <input type="checkbox" id="product_chk"><label for="product_chk"></label>
-          </div>
-        </div>
-        <div class="item">
-          <p>작가 이름</p>
-          <div class="product">
-            <img class="product-img" src="https://i.pinimg.com/originals/cf/34/f0/cf34f0af5496bf6eadda07c8042b9279.png" alt="상품 사진">
-          </div>
-        </div>
-        <div class="item">
-          <div class="product-info">
-            <ul>
-              <li>작품명</li>
-              <li>작가명</li>
-              <li>작품형식</li>
-            </ul>
-          </div>
-        </div>
-        <div class="item">
-            <div class="updown"></div>
-        </div>
-        <div class="item">
-          <div class="cost">
-            <ul>
-              <li>가격</li>
-              <li>작품 가격</li>
-              <li>배송비</li>
-            </ul>
-          </div>
-        </div>
-        <div class="item">
-          <div class="check">
-            <input type="checkbox" id="product_chk"><label for="product_chk"></label>
-          </div>
-        </div>
-        <div class="item">
-          <p>작가 이름</p>
-          <div class="product">
-            <img class="product-img" src="https://i.pinimg.com/originals/cf/34/f0/cf34f0af5496bf6eadda07c8042b9279.png" alt="상품 사진">
-          </div>
-        </div>
-        <div class="item">
-          <div class="product-info">
-            <ul>
-              <li>작품명</li>
-              <li>작가명</li>
-              <li>작품형식</li>
-            </ul>
-          </div>
-        </div>
-        <div class="item">
-            <div class="updown"></div>
-        </div>
-        <div class="item">
-          <div class="cost">
-            <ul>
-              <li>가격</li>
-              <li>작품 가격</li>
-              <li>배송비</li>
-            </ul>
-          </div>
-        </div>
-      	<div class="item">
-          <input type="checkbox" id="all_chk"><label for="all_chk">전체 상품 선택</label>
-        </div>
-        <div class="item total-cost">
-          <div class="total">
-            <ul>
-              <li>총 상품 금액</li>
-              <li>30,000원</li>
-            </ul>
-          </div>
-          <div class="add">
-            <p>+</p>
-          </div>
-          <div class="shipping-fee">
-            <ul>
-              <li>배송비</li>
-              <li>0원</li>
-            </ul>
-          </div>
-          <div class="sub">
-            <p>-</p>
-          </div>
-          <div class="discount">
-            <ul>
-              <li>예상 할인 금액</li>
-              <li><span style="color:#FF2600">2,000</span></li>
-            </ul>
-          </div>
-        </div>
-      	<div class="item">
-            <div class="updown"></div>
-        </div>
-      	<div class="item">
-          <div class="order-total">
-            <ul>
-              <li>총 주문 금액</li>
-              <li><span style="color:#FF2600">28,000</span></li>
-            </ul>
-          </div>
-        </div>
-				<div class="item">
-					<div class="purchase">
-						<button id="purchase-btn" onclick="javascript:btn()"> 구매하기 </button>
-						<!-- <p>구매하기</p> -->
+			<div class="grid-box">
+				<div class="area">
+					<div class="title">
+						<p>구매 예정 상품</p>
+					</div>
 				</div>
-      </div>
-		 </div>
+				<div class="area">
+					<div class="info">
+						<div class="profile-img"><img id="profile-img" src="https://i.pinimg.com/originals/cf/34/f0/cf34f0af5496bf6eadda07c8042b9279.png" alt="프로필 사진">
+						</div>
+						<div class="art-info">
+							<ul>
+								<li id="name">작품명</li>
+								<li id="author">작가명</li>
+							</ul>
+						</div>
+						<div class="price">
+							<ul>
+								<li id="price">가격</li>
+								<li id="art-price">작품 가격</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="area">
+					<div class="purchase-button">
+						<div class="purchase">
+							<button id="purchase-btn" onclick="javascript:btn()"> 결제하기 </button>
+							<!-- <p>구매하기</p> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<!-- 메인 끝 -->
 		<!-- 푸터 시작 -->
-		<div class="footer">
-			<!-- <address>(14097)경기도 안양시 만안구 성결대학로 53 TEL (031)467-8114
-			</address> -->
+		<!-- <div class="footer">
 			<p>Copyright(c)2021 Intact㈜ Intact Co., Ltd. All Rights Reserved</p>
-		</div>
+		</div> -->
 		<!-- 푸터 끝-->
 	</div>
-	<script> function btn(){ alert('구매가 완료되었습니다.'); } </script>
+	<script> function btn(){ alert('결제가 완료되었습니다.'); } </script>
 </body>
 </html>
